27849cf6c17668a6ac85b89ee1dea1e50b2de8a0
Maximilian Hanussek <maximilianhanussek@u-163-c089.vpn.uni-tuebingen.de>
2021-01-22 15:58:00 +0100

    Updated Dockerfile and corresponding scripts to new tools

--- docker/install_check.sh
+++ docker/install_check.sh
@@ -76,15 +76,45 @@ path="datasets/ebi/DRR001012.fastq"
 check_files "$path"
 path="datasets/ebi/DRR001012.fa"
 check_files "$path"
+path="datasets/BWA/DRR001012/DRR001012.amb"
+check_files "$path"
+path="datasets/BWA/DRR001012/DRR001012.ann"
+check_files "$path"
+path="datasets/BWA/DRR001012/DRR001012.bwt"
+check_files "$path"
+path="datasets/BWA/DRR001012/DRR001012.pac"
+check_files "$path"
+path="datasets/BWA/DRR001012/DRR001012.sa"
+check_files "$path"
 
 # DRR001025
 path="datasets/ebi/DRR001025.fastq"
 check_files "$path"
 path="datasets/ebi/DRR001025.fa"
 check_files "$path"
+path="datasets/BWA/DRR001025/DRR001025.amb"
+check_files "$path"
+path="datasets/BWA/DRR001025/DRR001025.ann"
+check_files "$path"
+path="datasets/BWA/DRR001025/DRR001025.bwt"
+check_files "$path"
+path="datasets/BWA/DRR001025/DRR001025.pac"
+check_files "$path"
+path="datasets/BWA/DRR001025/DRR001025.sa"
+check_files "$path"
 
 # GRCh38
 path="datasets/1000_genomes/GRCh38_full_analysis_set_plus_decoy_hla.fa"
+check_files "$path"
+path="datasets/BWA/GRCh38_full_analysis_set_plus_decoy_hla/GRCh38_full_analysis_set_plus_decoy_hla.amb"
+check_files "$path"
+path="datasets/BWA/GRCh38_full_analysis_set_plus_decoy_hla/GRCh38_full_analysis_set_plus_decoy_hla.ann"
+check_files "$path"
+path="datasets/BWA/GRCh38_full_analysis_set_plus_decoy_hla/GRCh38_full_analysis_set_plus_decoy_hla.bwt"
+check_files "$path"
+path="datasets/BWA/GRCh38_full_analysis_set_plus_decoy_hla/GRCh38_full_analysis_set_plus_decoy_hla.pac"
+check_files "$path"
+path="datasets/BWA/GRCh38_full_analysis_set_plus_decoy_hla/GRCh38_full_analysis_set_plus_decoy_hla.sa"
 check_files "$path"
 
 # wgs.ANCA.1_200.fsa
@@ -95,8 +125,28 @@ check_files "$path"
 path="datasets/clustalOmega/wgs.ANCA.1_400.fsa"
 check_files "$path"
 
-# wgs.ANCA.1_400.fsa
-path="datasets/clustalOmega/wgs.ANCA.1_400.fsa"
+# wgs.ANCA.1_500.fsa
+path="datasets/clustalOmega/wgs.ANCA.1_500.fsa"
+check_files "$path"
+
+# OE-38_R1
+path="datasets/SINA/OE-38_R1.fa"
+check_files "$path"
+path="datasets/SINA/OE-38_R1.fastq"
+check_files "$path"
+
+# RefSeq-RDP16S_v2_May2018
+path="datasets/SINA/RefSeq-RDP16S_v2_May2018.fa"
+check_files "$path"
+
+# GTDB_bac-arc_ssu_r8
+path="datasets/SINA/GTDB_bac-arc_ssu_r86.fa"
+check_files "$path"
+
+# SILVA_138.1_SSURef_NR99_12_06_20_opt
+path="datasets/SINA/SILVA_138.1_SSURef_NR99_12_06_20_opt.arb"
+check_files "$path"
+path="datasets/SINA/SILVA_138.1_SSURef_NR99_12_06_20_opt.sidx"
 check_files "$path"
 
 # nmonchart
@@ -126,15 +176,30 @@ cmd="gcc/gcc-installed/bin/gcc --version"
 name="GCC 7.3.0"
 check_tool "$cmd" "$name"
 
+# BBMap
+cmd="BBMap/bbmap/bbmap.sh"
+name="BBMap"
+check_tool "$cmd" "$name"
+
 # Bowtie2 build
-cmd="bowtie2/bowtie2-2.3.4.2/bowtie2-build --version"
+cmd="bowtie2/bowtie2-2.4.2/bowtie2-build --version"
 name="Bowtie2 build"
 check_tool "$cmd" "$name"
 
-#  Bowtie2 align
-cmd="bowtie2/bowtie2-2.3.4.2/bowtie2 --version"
+# Bowtie2 align
+cmd="bowtie2/bowtie2-2.4.2/bowtie2 --version"
 name="Bowtie2 align"
 check_tool "$cmd" "$name"
+
+# BWA
+version_line=$(BWA/bwa-0.7.17/bwa 2>&1 | grep Version)
+name="BWA"
+if [ "$version_line" == "Version: 0.7.17-r1188" ]
+then
+        echo -e "${green}$name seems to be installed correctly.${nc}"
+else
+        echo -e "${red}Something seems to be wrong with $name. Please check and maybe reinstall again.${nc}"
+fi
 
 # Velveth
 cmd="velvet/velveth"
@@ -188,3 +253,19 @@ check_tool "$cmd" "$name"
 cmd="clustalOmega/clustal-omega-1.2.4/bin/clustalo --version"
 name="ClustalOmega"
 check_tool "$cmd" "$name"
+
+# MAFFT
+version_line=$(MAFFT/mafft-7.475-with-extensions/bin/mafft --help 2>&1 | grep MAFFT)
+name="MAFFT"
+if [ "$version_line" == "  MAFFT v7.475 (2020/Nov/23)" ]
+then
+        echo -e "${green}$name seems to be installed correctly.${nc}"
+else
+        echo -e "${red}Something seems to be wrong with $name. Please check and maybe reinstall again.${nc}"
+fi
+
+# SINA
+cmd="SINA/sina-1.7.2-linux/sina -V"
+name="SINA"
+check_tool "$cmd" "$name"
+
